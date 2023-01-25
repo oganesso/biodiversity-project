700ec991307853c51c0531650c185b6b346f9e4f
treetreetreee <63928357+treetreetreee@users.noreply.github.com>
2023-01-24 22:07:54 -0500

    push

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # biodiversity-project
  for Codecademy data analysis course
+
+
+# Introduction:
+
+In this porject are CSV files for conservation status and population count for a set of species.
+Goal: Interpret data from the National Parks Service about endangered species in different parks.
+
+Investigate if there are any patterns or themes to the types of species that become endangered. Analyze, then clean up, plot data as well as pose questions and seek to answer them in a meaningful way.
+
+# Conclusions and takeaways:
+
+Q: What did you learn throughout the process?
+A: The number of species in a state of concern, the spread of organism observations throughout parks sorted by species, spread of conservations statuses, the spread of conservation statuses sorted by category of organism, which species are most sighted at each park.
+
+Q: Are the results what you expected?
+A: A surprisingly few amount of species have a conservation status. Of the species that are of conservation status at some level, the vast majority are birds or vascular plants. Of the species that do have a conservation status, the vast majority are in the lowest status, "Species of Concern". Only 25-30 species are in any higher level of danger.
+
+Q: What are the key findings and takeaways?
+A: Out of 5824 species, 191 have a conservation status. Of those 191, 25-30 are Endangered, Threatened, or In Recovery. Of all organism types, bird species make up the vast majority of species that are in concservation status. Most species in the "Species of Concern" category are of the bird and vascular plant categories. Most species in the "In Recovery" category are birds.
---
+++ data-analysis.py
@@ -0,0 +1,59 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+# import data
+species_info = pd.read_csv("species_info.csv")
+observations = pd.read_csv("observations.csv")
+
+# calculate summary stats
+#   species info
+unique_organism_categories = species_info.category.unique()
+num_unique_organism_categories = species_info.category.nunique()
+unique_conservation_status_categories = species_info.conservation_status.unique()
+# num_of_no_concern_species = (species_info[species_info.conservation_status == np.nan]).count()
+other_num_of_species = (species_info["conservation_status"]).count()
+#   observations
+unique_park_names = observations.park_name.unique()
+median_observations = observations.observations.median()
+mean_observations = observations.observations.mean()
+observations_std_dev = observations.observations.std()
+observations_first_quartile = observations.observations.quantile(.25)
+observations_third_quartile = observations.observations.quantile(.75)
+observation_duplciates = observations[observations.duplicated() == True]
+
+
+# return summary stats
+#   species info
+print("\nunique_organism_categories:", unique_organism_categories)
+print("num_unique_organism_categories:", num_unique_organism_categories)
+print("unique_conservation_status_categories:", unique_conservation_status_categories)
+# print("num_of_no_concern_species", num_of_no_concern_species)
+print("other_num_of_species", other_num_of_species)
+#   observations
+print("unique_park_names", unique_park_names)
+print("median_observations", median_observations)
+print("mean_observations", mean_observations)
+print("observations_std_dev", observations_std_dev)
+print("observations_first_quartile", observations_first_quartile)
+print("observations_third_quartile", observations_third_quartile)
+
+# visual examination
+#   What is the distribution of conservation_status for animals?
+sns.countplot(data=species_info, x="conservation_status")
+plt.title("Spread of conservation status among species of concern")
+plt.show()
+plt.clf()
+#   Are certain types of species more likely to be endangered?
+sns.countplot(data=species_info, x="category", hue="conservation_status")
+ax = plt.subplot()
+ax.set_xticklabels(unique_organism_categories, rotation=10)
+plt.title("Spread of conservation status among species of concern, sorted")
+plt.show()
+plt.clf()
+#   Which species were spotted the most at each park?
+observations.drop_duplicates(inplace=True, subset=["scientific_name"])
+observations_sorted_by_park = observations.pivot(columns="park_name", index="scientific_name", values="observations")
+for column in observations_sorted_by_park.columns:
+    observations_sorted_by_park.sort_values(by=[column], ascending=False, inplace=True)
+    print("\n\n" + "Most sighted species in", column + ":\n"), print(observations_sorted_by_park.head())
